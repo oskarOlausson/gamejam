0d67d12da73edec3d878c0737f183d9ec55da157
meris.bahti@gmail.com <meris.bahti@gmail.com>
2020-07-12 19:23:00 +0200

    add long shot

--- src/levels/index.ts
+++ src/levels/index.ts
@@ -1,3 +1,4 @@
 export * from './the-woods'
 export * from './wavy-woods'
+export * from './long-shot'
 export * from './woods-tight-trees'
---
+++ src/levels/long-shot.ts
@@ -0,0 +1,30 @@
+import { Level } from '../state'
+import { createWindState } from '../wind'
+import { W, H } from '../constants'
+
+export const longShot: Level = {
+  trees: [
+    { center: [30, 30], radius: 20 },
+    { center: [W - 30, H - 30], radius: 20 },
+  ],
+  nrShots: 0,
+  par: 1,
+  wind: createWindState(
+    [Math.random() * 2 - 1, Math.random() * 2 - 1],
+    [Math.random() * 2 - 1, Math.random() * 2 - 1],
+    0,
+  ),
+  basket: {
+    center: [W - 40, 80],
+    radius: 30,
+  },
+  wonAt: null,
+  disc: {
+    wind: [],
+    center: [40, H - 80],
+    travel: [],
+    travelStart: 0,
+    radius: 20,
+    lastShot: [40, H - 80],
+  },
+}
